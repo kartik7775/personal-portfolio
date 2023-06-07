2c4348d5aaa19876445ab5c33127777c006b0890
Kartik Nikam <krt1993@outlook.com>
2023-06-08 02:35:35 +0530

    styles and responsiveness modified

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -12,44 +12,48 @@ const data = [
   {
     id: 1,
     image: IMG1,
-    title: "imagenerator : AI Powered Image Generator Full Stack Application",
+    title: "imagenerator",
+    info: "AI Powered Image Generator Full Stack Application",
     github: "https://github.com/kartik7775/imagenerator",
     demo: "https://imagenerator-ai.vercel.app/",
-
-    
   },
   {
     id: 2,
     image: IMG2,
-    title: "getPrompts : AI Prompts Sharing Web Application",
+    title: "getPrompts",
+    info: "AI Prompts Sharing Web Application",
     github: "https://github.com/kartik7775/getPrompts",
     demo: "https://get-prompts.vercel.app/",
   },
   {
     id: 3,
     image: IMG3,
-    title: "Guess The Number : A Fun Game Application",
+    title: "Guess The Number",
     github: "https://github.com/kartik7775/Guess-The-Number-Game",
+    info: "A fun for all, number geussing game application.",
     demo: "https://numberguessing.vercel.app/",
   },
   {
     id: 4,
     image: IMG4,
-    title: "Quizzeroma : A Quiz / Trivia Game Application",
+    title: "Quizzeroma",
+    info: "A Quiz / Trivia Game Application",
     github: "https://github.com/kartik7775/quizzeroma",
     demo: "https://quizzeroma.netlify.app/",
   },
   {
     id: 5,
     image: IMG5,
-    title: "Disney+ : Website Clone (currently working on)",
+    title: "Disney+",
+    info: "Website Clone (currently working on)",
     github: "https://github.com/kartik7775",
     demo: "https://dribbble.com/kartikN775/collections",
   },
   {
     id: 6,
     image: IMG6,
-    title: "Car Showroom : Landing Page",
+    title: "Car Showroom",
+    info: "Landing Page",
     github: "https://github.com/kartik7775",
     demo: "https://dribbble.com/kartikN775/collections",
   },
@@ -62,25 +66,36 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        {data.map(({ id, image, title, github, demo }) => {
+        {data.map(({ id, image, title, info, github, demo }) => {
           return (
             <article key={id} className="portfolio__item">
-
               <div className="portfolio__item-image">
                 <img src={image} alt="title" />
               </div>
 
-              <h3>{title}</h3>
+              <div className="title-info">
+                <h3>{title}</h3>
+                <p>{info}</p>
+              </div>
 
               <div className="portfolio__item-cta">
-                <a href={github} className="btn border" target="_blank" rel="noreferrer">
-                  GitHub Code
+                <a
+                  href={github}
+                  className="btn border"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  View Code
                 </a>
-                <a href={demo} className="btn btn-primary" target="_blank" rel="noreferrer">
+                <a
+                  href={demo}
+                  className="btn btn-primary"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   Live Demo
                 </a>
               </div>
-              
             </article>
           );
         })}
